1a4ae6e12cd9eba13efbcdf285409c1e2992678c
yassine elmiri <yassireelhima@gmail.com>
2024-09-05 23:24:07 +0100

    Created : Home page with Formateur/Etudiant selection and profile insertion functionality Added functionality for inserting profiles (Formateur/Etudiant) into the database Developed forms for profile creation and connected them to the backend Implemented navigation between Home page and profile insertion pages

---
+++ db.js
@@ -0,0 +1,24 @@
+const mysql = require("mysql");
+
+const db = mysql.createConnection({
+    host: process.env.MYSQL_HOST || "localhost",  // Changed from "db" to "localhost"
+    user: process.env.MYSQL_USER || "root",
+    password: process.env.MYSQL_PASSWORD || "admin",
+    database: process.env.MYSQL_DATABASE || "jcc",
+  });
+  
+
+function connectWithRetry() {
+  db.connect((err) => {
+    if (err) {
+      console.error("Error connecting to MySQL:", err.stack);
+      setTimeout(connectWithRetry, 2000);
+    } else {
+      console.log("Connected to MySQL");
+    }
+  });
+}
+
+connectWithRetry();
+
+module.exports = db;
